8ef8b5cc9710fcc0a205996ac6e653fc5435f97e
SharayuMane1610 <89628626+SharayuMane1610@users.noreply.github.com>
2021-09-16 12:57:54 +0530

    add

---
+++ leap.py
@@ -0,0 +1,16 @@
+year=int(input("Enter year"))
+if (year%4)==0:
+    if(year%100)==0:
+            if(year %400)==0:
+                print("leap year")
+            else:
+                print("not leap")
+    else:
+        print("leap year")
+            
+else:
+    print("not leap year")
+        
+            
+
+        
